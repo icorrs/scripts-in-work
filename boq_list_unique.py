1042d44d68f478d3f99e5250465ca97d76fe306f
zongjie-mao <mao_zongjie@sina.com>
2018-06-17 03:13:08 +0800

    not only be used in chanpter 500
    in re compile pat ,change  begin with '5' to ('1','2','4'),so can be used in chanpter 100,200,400.

--- boq_list_unique.py
+++ boq_list_unique.py
@@ -3,17 +3,18 @@ import numpy as np
 import pandas as pd
 import re
 import pprint
-file_route=input('please enter file route:')
-wb=openpyxl.load_workbook(file_route)
-sh=wb.get_sheet_by_name('隧道')
-list_source=[]
-list_dir=[]
-f=open(r'c:\python tem\boq_list.txt','w',encoding='utf-8')
-l=open(r'c:\python tem\boq_list_noindex.txt','w',encoding='utf-8')
-for i in range(149):
-    list_source.append(sh['a'+str(i+2)].value)
+file_route = input('please enter file route:')
+wb = openpyxl.load_workbook(file_route)
+sh = wb.get_sheet_by_name('01')
+list_source = []
+list_dir = []
+sub_word = ''
+f = open(r'c:\python tem\boq_list.txt','w',encoding='utf-8')
+l = open(r'c:\python tem\boq_list_noindex.txt','w',encoding='utf-8')
+for i in range(1,471):
+    list_source.append(sh['a'+str(i)].value)
 for i in range(len(list_source)):
-    if list(list_source[i])[0]=='5':
+    if list_source[i][0] in ('1','2','4'):
         sub_word=list_source[i]
         list_dir.append(sub_word)
     else:
